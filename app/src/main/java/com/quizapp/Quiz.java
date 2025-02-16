935e283b3bbd48d7543ae40759520223e0598993
Talha Khan <mtalhak93@gmail.com>
2025-02-16 19:03:00 +0500

    Created Home screen

--- app/src/main/java/com/quizapp/Quiz.java
+++ app/src/main/java/com/quizapp/Quiz.java
@@ -1,9 +1,6 @@
 package com.quizapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Home extends AppCompatActivity {
-    private EditText etName;
-    private Button btnStart;
+public class Quiz extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_quiz);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        init();
-        btnStart.setOnClickListener((s)->{
-            String name = etName.getText().toString();
-            Intent intent = new Intent(
-                    Home.this,
-                    Quiz.class
-            );
-            intent.putExtra("name",name);
-            startActivity(intent);
-        });
-    }
-    private void init(){
-        etName = findViewById(R.id.et_name);
-        btnStart = findViewById(R.id.btn_start);
     }
 }
